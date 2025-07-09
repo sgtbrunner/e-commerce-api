a94e015c2ea8512486dbaab081e2e92a25605f7e
Guilherme Brunner <brunner@AE-BRA-10166.local>
2025-07-09 19:04:59 -0300

    chore: connects to DB

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+
+// express
+const express = require("express");
+const app = express();
+
+// database
+const connectDB = require("./db/connect");
+
+const PORT = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL).then(() =>
+      app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
+    );
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+start();
