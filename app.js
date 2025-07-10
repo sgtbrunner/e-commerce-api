beb104c7c4e9046e859701bff451c7c65095ced6
Guilherme Brunner <brunner@AE-BRA-10166.local>
2025-07-10 18:59:31 -0300

    feat: setups middlewares

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
+require("express-async-errors");
 
 // express
 const express = require("express");
 const app = express();
 
+// rest of the packages
+const morgan = require("morgan");
+
 // database
 const connectDB = require("./db/connect");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => res.status(200).send("e-commerce API"));
 
 const PORT = process.env.PORT || 5000;
 const start = async () => {
