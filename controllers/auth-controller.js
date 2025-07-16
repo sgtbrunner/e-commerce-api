594e2c80a3be98be31dd0df8382c99d86516805e
Guilherme Brunner <brunner@AE-BRA-10166.local>
2025-07-15 21:01:26 -0300

    feat: setups auth/register endpoint

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,24 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const register = async (req, res) => {
-  return res.send('register user');
+  const { email, name, password } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+
+  if (emailAlreadyExists)
+    throw new CustomError.BadRequestError('Email already exists');
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role: isFirstAccount ? 'admin' : 'user',
+  });
+  return res.status(StatusCodes.CREATED).send({ user });
 };
 
 const login = async (req, res) => {
